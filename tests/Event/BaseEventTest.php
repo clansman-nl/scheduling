e5d5ab23c5b94b207b8083d69ab8c23d3beded8b
Sebastiaan Hilbers <bashilbers@gmail.com>
2017-06-09 16:25:11 +0200

    fix getMock() issue in tests

--- tests/Event/BaseEventTest.php
+++ tests/Event/BaseEventTest.php
@@ -7,7 +7,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
     /**
      * @return BaseEvent|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected function getMock()
+    protected function getEvent()
     {
         return $this->getMockBuilder(BaseEvent::class)->getMockForAbstractClass();
     }
@@ -17,7 +17,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_every_minute()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
 
         $event->everyMinute();
 
@@ -29,7 +29,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_n_minutes()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->everyNMinutes(5);
 
         $this->assertSame("*/5 * * * * *", (string) $event->getCronExpression());
@@ -40,7 +40,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_hourly()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->hourly();
 
         $this->assertSame('0 * * * * *', (string) $event->getCronExpression());
@@ -51,7 +51,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_on_every_hour()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->hour(1);
 
         $this->assertSame('* 1 * * * *', (string) $event->getCronExpression());
@@ -62,7 +62,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_daily()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->daily();
 
         $this->assertSame('0 0 * * * *', (string) $event->getCronExpression());
@@ -73,7 +73,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_daily_at_a_specific_time()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->dailyAt('10:05');
         $this->assertSame('5 10 * * * *', (string) $event->getCronExpression());
 
@@ -86,7 +86,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_on_specific_days()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
 
         $event->days([1, 2 ,3 ]);
         $this->assertSame('* * * * 1,2,3 *', (string) $event->getCronExpression());
@@ -100,7 +100,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_on_weekdays_only()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->weekdays();
 
         $this->assertSame('* * * * 1-5 *', (string) $event->getCronExpression());
@@ -111,7 +111,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_weekly()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->weekly();
 
         $this->assertSame('0 0 * * 0 *', (string) $event->getCronExpression());
@@ -122,7 +122,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_monthly()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->monthly();
 
         $this->assertSame('0 0 1 * * *', (string) $event->getCronExpression());
@@ -133,7 +133,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_quarterly()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->quarterly();
 
         $this->assertSame('0 0 1 */3 * *', (string) $event->getCronExpression());
@@ -144,7 +144,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_can_run_yearly()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->yearly();
 
         $this->assertSame('0 0 1 1 * *', (string) $event->getCronExpression());
@@ -155,7 +155,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
      */
     function it_allows_for_filtering()
     {
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->everyMinute();
 
         $this->assertTrue($event->isDue());
@@ -166,7 +166,7 @@ class BaseEventTest extends \PHPUnit_Framework_TestCase
 
         $this->assertFalse($event->isDue());
 
-        $event = $this->getMock();
+        $event = $this->getEvent();
         $event->everyMinute();
 
         $this->assertTrue($event->isDue());
